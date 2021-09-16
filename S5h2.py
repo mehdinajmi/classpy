7ef3fb22da411c73abbbea2388f5bbbc4b04b367
Mehdi03111976 <86189183+mehdinajmi@users.noreply.github.com>
2021-09-16 19:14:36 +1000

    Add files via upload

---
+++ S5h2.py
@@ -0,0 +1,6 @@
+### to print names sitting in a lsit in an opposite way using one line script
+### Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
+a=['noshin']
+b=['majid','mehdi','mohamad']
+c=['kahdijeh', 'ahmad']
+print(a+b+c[-1:2])
