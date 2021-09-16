0c9b0e819b54e59aeaf25326753ab3ef52db0d40
Mehdi03111976 <86189183+mehdinajmi@users.noreply.github.com>
2021-09-16 19:05:14 +1000

    Add files via upload

---
+++ S5h1.py
@@ -0,0 +1,7 @@
+### to print names siiting in a lsit in a opposite way
+### Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
+a=['noshin']
+b=['majid','mehdi','mohamad']
+c=['kahdijeh', 'ahmad']
+d=a+b+c
+print(d[:-1:2])
