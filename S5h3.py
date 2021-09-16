70ebf634b7923df91726d0bf116fd616ac73eec1
Mehdi03111976 <86189183+mehdinajmi@users.noreply.github.com>
2021-09-16 19:06:25 +1000

    Add files via upload

---
+++ S5h3.py
@@ -0,0 +1,4 @@
+### to show a list of individual fruits within a lsit
+### Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
+fruits=['apple', 'melon','apple','watermelon','melon','berry','pear','peach','berry']
+print(set(fruits))
