c55c647b8afe9a995104ebad4e65fe4f2b50ddd6
Mehdi03111976 <86189183+mehdinajmi@users.noreply.github.com>
2021-09-23 18:27:51 +1000

    Add files via upload

---
+++ S6h5.py
@@ -0,0 +1,14 @@
+#To sum of numbers between 1 to 1000 the remainings divided by 3 is equal to zero
+# Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
+list_adad=[]
+list_position=0
+adad=0
+while adad <=1000:
+
+    if adad %3==0:
+        list_adad.append(adad)
+    adad+=1
+print(list_adad)
+print('The Total number is:' ,sum(list_adad))
+print(len(list_adad))
+print('the average number of list is equal to:' ,sum(list_adad)/len(list_adad))
