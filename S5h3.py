7a0605054069640a2a431dc3344b351d5524e37f
Mehdi03111976 <86189183+mehdinajmi@users.noreply.github.com>
2025-01-05 18:42:00 +1100

    Update S5h3.py via github

--- S5h3.py
+++ S5h3.py
@@ -2,3 +2,4 @@
 ### Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
 fruits=['apple', 'melon','apple','watermelon','melon','berry','pear','peach','berry']
 print(set(fruits))
+this line is added on 05/01/2024 @ 6:41 pm
