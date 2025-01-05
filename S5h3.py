a378fe1de6287ce69bd6723461f59b20f1438b52
Mehdi Najmi <mehdinajmi@yahoo.com.au>
2025-01-05 17:27:28 +1100

    make change in filw S5h3.py

--- S5h3.py
+++ S5h3.py
@@ -2,3 +2,4 @@
 ### Mehdi Najmi- sematec Python class - Thursday 2:00 pm to 6:00 pm
 fruits=['apple', 'melon','apple','watermelon','melon','berry','pear','peach','berry']
 print(set(fruits))
+ this line is added on 05/01/2025 @ 5:25 pm
